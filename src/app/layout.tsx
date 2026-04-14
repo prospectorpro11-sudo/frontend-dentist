94677899edf902e2d8fbf61139072c9f263f634f
Samiul Hasan <samiulxhasan650@gmail.com>
2026-04-14 13:16:51 +0600

    Project Initialize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/globals.scss";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
