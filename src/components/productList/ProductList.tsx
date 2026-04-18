f58074b833f82834491aea874a7fd09acd467a2b
Samiul Hasan <samiulxhasan650@gmail.com>
2026-04-18 11:06:44 +0600

    Changes

--- src/components/productList/ProductList.tsx
+++ src/components/productList/ProductList.tsx
@@ -17,6 +17,7 @@ import { IProductList } from '../../shared/interface';
 import { COLORS } from '@/shared/colors';
 import Button from '../button/Button';
 import { BUTTON_SIZE_ENUM, BUTTON_VARIANT_ENUM } from '@/shared/enums';
+import { Col, Container } from 'react-bootstrap';
 
 const productIcons = {
     tooth: FaTooth,
@@ -38,7 +39,7 @@ const ProductList = (props: IProductList) => {
 
     return (
         <section className={styles.productList}>
-            <div className={styles.productListContainer}>
+            <Container>
 
                 {/* Background decorations */}
                 <div className={classNames(styles.plBlob, styles.plBlob1)}></div>
@@ -72,51 +73,52 @@ const ProductList = (props: IProductList) => {
                 </div>
 
                 {/* 4-Column Product Table */}
-                <div className={styles.plTableSection}>
-                    <div className={styles.plTableCardNew}>
+                <Col xs={12} lg={10} className='mx-auto'>
+                    <div className={styles.plTableSection}>
+                        <div className={styles.plTableCardNew}>
 
-                        {/* Table Header Bar */}
-                        <div className={styles.plNewTableHead}>
-                            <div className={styles.plNewTh} style={{ flex: 1.2 }}>Product Category</div>
-                            <div className={styles.plNewTh} style={{ flex: 0.8 }}>Contacts</div>
-                            <div className={styles.plNewTh} style={{ flex: 0.8, textAlign: 'right' }}>Action</div>
-                        </div>
+                            {/* Table Header Bar */}
+                            <div className={styles.plNewTableHead}>
+                                <div className={styles.plNewTh} style={{ flex: 1.2 }}>Product Category</div>
+                                <div className={styles.plNewTh} style={{ flex: 0.8 }}>Contacts</div>
+                                <div className={styles.plNewTh} style={{ flex: 0.8, textAlign: 'right' }}>Action</div>
+                            </div>
 
-                        {/* Table Body */}
-                        <div className={styles.plNewTableBody}>
+                            {/* Table Body */}
+                            <div className={styles.plNewTableBody}>
 
-                            {products.map((product, index) => {
-                                const Icon = productIcons[product.icon as keyof typeof productIcons];
-                                return (
-                                    <div key={index} className={styles.plNewRow}>
-                                        <div className={styles.plNewCell} style={{ flex: 1.2 }}>
-                                            <div className={classNames(styles.plCatIcon, styles[product.colorClass])}>
-                                                <Icon />
+                                {products.map((product, index) => {
+                                    const Icon = productIcons[product.icon as keyof typeof productIcons];
+                                    return (
+                                        <div key={index} className={styles.plNewRow}>
+                                            <div className={styles.plNewCell} style={{ flex: 1.2 }}>
+                                                <div className={classNames(styles.plCatIcon, styles[product.colorClass])}>
+                                                    <Icon />
+                                                </div>
+                                                <div className={styles.plCatInfo}>
+                                                    <strong>{product.category}</strong>
+                                                    <span>{product.description}</span>
+                                                </div>
                                             </div>
-                                            <div className={styles.plCatInfo}>
-                                                <strong>{product.category}</strong>
-                                                <span>{product.description}</span>
+                                            <div className={styles.plNewCell} style={{ flex: 0.8 }}>
+                                                <div className={styles.plCountBadge}>
+                                                    <BsPeopleFill color={COLORS.PRIMARY} />
+                                                    <strong>{product.count}</strong>
+                                                </div>
+                                            </div>
+                                            <div className={classNames(styles.plNewCell, styles.plActionCell)} style={{ flex: 0.8 }}>
+                                                <a href="#"><Button size={BUTTON_SIZE_ENUM.SMALL} variant={BUTTON_VARIANT_ENUM.PRIMARY_LIGHT}><BsCartPlus /> Add to Cart</Button></a>
+                                                <a href="#" className={styles.plCustomizeLink}>Customize</a>
                                             </div>
                                         </div>
-                                        <div className={styles.plNewCell} style={{ flex: 0.8 }}>
-                                            <div className={styles.plCountBadge}>
-                                                <BsPeopleFill color={COLORS.PRIMARY} />
-                                                <strong>{product.count}</strong>
-                                            </div>
-                                        </div>
-                                        <div className={classNames(styles.plNewCell, styles.plActionCell)} style={{ flex: 0.8 }}>
-                                            <a href="#"><Button size={BUTTON_SIZE_ENUM.SMALL} variant={BUTTON_VARIANT_ENUM.PRIMARY_LIGHT}><BsCartPlus /> Add to Cart</Button></a>
-                                            <a href="#" className={styles.plCustomizeLink}>Customize</a>
-                                        </div>
-                                    </div>
-                                );
-                            })}
+                                    );
+                                })}
 
+                            </div>
                         </div>
                     </div>
-                </div>
-
-            </div>
+                </Col>
+            </Container>
         </section>
     );
 };
