d2c3d1d75bbbad7f3aa38106fc69996adebb065d
Samiul Hasan <samiulxhasan650@gmail.com>
2026-04-20 18:27:29 +0600

    Changes

--- src/mainViews/productDetailsMainView/ProductDetailsMainView.tsx
+++ src/mainViews/productDetailsMainView/ProductDetailsMainView.tsx
@@ -1,17 +1,17 @@
+import Faq from "@/components/faq/Faq";
 import FreeSample from "@/components/freeSample/FreeSample";
-import { PRODUCT_DETAILS_SEED_OBJECT } from "@/shared/seeds/productDetailsSeeds";
-import ProductDetailsBanner from "./views/productDetailsBanner/ProductDetailsBanner";
-import WhatsIncludedDetails from "./views/whatsIncludedDetails/WhatsIncludedDetails";
-import ProductPriceList from "./views/productPriceList/ProductPriceList";
-import CustomDentistList from "@/components/customDentistList/CustomDentistList";
 import WhyChooseUs from "@/components/whyChooseUs/WhyChooseUs";
-import DentalSpecialtyList from "@/components/dentalSpecialtyList/DentalSpecialtyList";
 import CrmIntegration from "@/components/crmIntegration/CrmIntegration";
 import VerifiedSource from "@/components/verifiedSource/VerifiedSource";
-import DataBeneficiaries from "@/components/dataBeneficiaries/DataBeneficiaries";
+import ProductPriceList from "./views/productPriceList/ProductPriceList";
 import ComparisonTable from "@/components/comparisonTable/ComparisonTable";
+import { PRODUCT_DETAILS_SEED_OBJECT } from "@/shared/seeds/productDetailsSeeds";
+import CustomDentistList from "@/components/customDentistList/CustomDentistList";
+import DataBeneficiaries from "@/components/dataBeneficiaries/DataBeneficiaries";
+import ProductDetailsBanner from "./views/productDetailsBanner/ProductDetailsBanner";
+import WhatsIncludedDetails from "./views/whatsIncludedDetails/WhatsIncludedDetails";
+import DentalSpecialtyList from "@/components/dentalSpecialtyList/DentalSpecialtyList";
 import AboutDentistEmailList from "@/components/aboutDentistEmailList/AboutDentistEmailList";
-import Faq from "@/components/faq/Faq";
 
 const ProductDetailsMainView = () => {
     return (
