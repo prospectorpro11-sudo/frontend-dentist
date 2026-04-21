472d9073985d6b86894bb687348793995547888e
Samiul Hasan <samiulxhasan650@gmail.com>
2026-04-21 11:03:49 +0600

    ComplianceDataIntegrity, JoinWithUs, OurMission Components

--- src/mainViews/aboutMainView/AboutMainView.tsx
+++ src/mainViews/aboutMainView/AboutMainView.tsx
@@ -1,10 +1,16 @@
+import ComplianceDataIntegrity from "@/components/complianceDataIntegrity/ComplianceDataIntegrity";
 import styles from "./aboutMainView.module.scss";
 import AboutBanner from "./views/AboutBanner";
+import JoinWithUs from "./views/joinWithUs/JoinWithUs";
+import OurMission from "./views/ourMission/OurMission";
 
 const AboutMainView = () => {
     return (
         <>
             <AboutBanner />
+            <OurMission />
+            <JoinWithUs />
+            <ComplianceDataIntegrity />
         </>
     );
 };
