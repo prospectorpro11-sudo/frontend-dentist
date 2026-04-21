f7af32f9495b077e9c93fe6b9fb3d5c1fb10d578
Samiul Hasan <samiulxhasan650@gmail.com>
2026-04-21 16:52:38 +0600

    Changes

--- src/prospector/components/prospectorStats/ProspectorStats.tsx
+++ src/prospector/components/prospectorStats/ProspectorStats.tsx
@@ -1,37 +1,28 @@
-import { FaAddressBook, FaEnvelope, FaFax, FaPhone } from "react-icons/fa6";
 import styles from "./prospectorStats.module.scss";
+import { numberWithCommas } from "@/shared/InternalService";
+import { FaAddressBook, FaEnvelope, FaFax, FaPhone } from "react-icons/fa6";
 
 const ProspectorStats = () => {
+    const statsData = [
+        { label: "Contacts", value: 930285, Icon: FaAddressBook },
+        { label: "Emails", value: 930285, Icon: FaEnvelope },
+        { label: "Phones", value: 930285, Icon: FaPhone },
+        { label: "Faxes", value: 930285, Icon: FaFax },
+    ];
+
     return (
         <div className={styles.stats}>
-            <div className={styles.stat}>
-                <div className={styles.sico}><FaAddressBook /></div>
-                <div>
-                    <div className={styles.sval}>930,285</div>
-                    <div className={styles.slab}>Contacts</div>
+            {statsData.map(({ label, value, Icon }) => (
+                <div key={label} className={styles.stat}>
+                    <div className={styles.sico}>
+                        <Icon />
+                    </div>
+                    <div>
+                        <div className={styles.sval}>{numberWithCommas(value.toString())}</div>
+                        <div className={styles.slab}>{label}</div>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.stat}>
-                <div className={styles.sico}><FaEnvelope /></div>
-                <div>
-                    <div className={styles.sval}>930,285</div>
-                    <div className={styles.slab}>Emails</div>
-                </div>
-            </div>
-            <div className={styles.stat}>
-                <div className={styles.sico}><FaPhone /></div>
-                <div>
-                    <div className={styles.sval}>930,285</div>
-                    <div className={styles.slab}>Phones</div>
-                </div>
-            </div>
-            <div className={styles.stat}>
-                <div className={styles.sico}><FaFax /></div>
-                <div>
-                    <div className={styles.sval}>930,285</div>
-                    <div className={styles.slab}>Faxes</div>
-                </div>
-            </div>
+            ))}
         </div>
     );
 };
