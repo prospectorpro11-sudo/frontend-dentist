633e41cde728327d605e42129e03c88cbcd234f6
Samiul Hasan <samiulxhasan650@gmail.com>
2026-04-22 16:10:54 +0600

    Dynamic Price

--- src/prospector/components/prospectorAddToCart/ProspectorAddToCart.tsx
+++ src/prospector/components/prospectorAddToCart/ProspectorAddToCart.tsx
@@ -2,11 +2,16 @@
 import { FaCartPlus } from "react-icons/fa6";
 import styles from "./prospectorAddToCart.module.scss";
 import { useProspectorContext } from "@/contexts/ProspectorContext";
-import { numberWithCommas } from "@/shared/InternalService";
+import { numberWithCommas, PROSPECTOR_PRODUCT_PRICE_UPDATE } from "@/shared/InternalService";
 import Skeleton from "react-loading-skeleton";
+import { useEffect } from "react";
 
 const ProspectorAddToCart = () => {
-    const { stats, prospectorLoading } = useProspectorContext();
+    const { stats, prospectorLoading, price, setPrice, emailAvailability } = useProspectorContext();
+    useEffect(() => {
+        const calculatePrice = PROSPECTOR_PRODUCT_PRICE_UPDATE((stats?.totalContacts || 0), (stats?.Unique_Emails || 0), emailAvailability?.value);
+        setPrice(calculatePrice);
+    }, [stats, emailAvailability, setPrice])
     return (
         <div className={styles.checkout}>
             <div className={styles.cprice}>
@@ -19,7 +24,7 @@ const ProspectorAddToCart = () => {
                     </>
                 ) : (
                     <>
-                        $452
+                        ${numberWithCommas(price.toString(), 0)}
                         <small>{numberWithCommas(stats?.totalContacts.toString() || "0")} contacts</small>
                     </>
                 )}
