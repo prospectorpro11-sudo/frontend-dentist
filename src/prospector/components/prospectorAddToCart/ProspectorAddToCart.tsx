d21a61c5bc1c1e6ab38f8360a2c68795f90013f2
Samiul Hasan <samiulxhasan650@gmail.com>
2026-04-22 11:11:24 +0600

    Price, Toatal Count and Button Loading

--- src/prospector/components/prospectorAddToCart/ProspectorAddToCart.tsx
+++ src/prospector/components/prospectorAddToCart/ProspectorAddToCart.tsx
@@ -1,11 +1,32 @@
+'use client';
 import { FaCartPlus } from "react-icons/fa6";
 import styles from "./prospectorAddToCart.module.scss";
+import { useProspectorContext } from "@/contexts/ProspectorContext";
+import { numberWithCommas } from "@/shared/InternalService";
+import Skeleton from "react-loading-skeleton";
 
 const ProspectorAddToCart = () => {
+    const { stats, prospectorLoading } = useProspectorContext();
     return (
         <div className={styles.checkout}>
-            <div className={styles.cprice}>$452<small>2,847 contacts</small></div>
-            <button className={styles.cartbtn} type="button"><FaCartPlus /> Add All to Cart</button>
+            <div className={styles.cprice}>
+                {prospectorLoading ? (
+                    <>
+                        <Skeleton width={80} height={15} />
+                        <small>
+                            <Skeleton width={95} height={10} />
+                        </small>
+                    </>
+                ) : (
+                    <>
+                        $452
+                        <small>{numberWithCommas(stats?.totalContacts.toString() || "0")} contacts</small>
+                    </>
+                )}
+            </div>
+            <button className={styles.cartbtn} type="button" disabled={prospectorLoading}>
+                <FaCartPlus /> {prospectorLoading ? "Loading..." : "Add All to Cart"}
+            </button>
         </div>
     );
 };
